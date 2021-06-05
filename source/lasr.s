b7622b0636aac7f7105ae3c0894d83dcd2fdd2ff
Felix Jones <felix@felixjones.co.uk>
2021-06-05 15:30:18 +0200

    Tabs to spaces

--- source/lasr.s
+++ source/lasr.s
@@ -11,10 +11,10 @@
     .global __aeabi_lasr
     .type __aeabi_lasr STT_FUNC
 __aeabi_lasr:
-	subs	r3, r2, #32
-	rsb	    r12, r2, #32
-	lsrmi	r0, r0, r2
-	asrpl	r0, r1, r3
-	orrmi	r0, r0, r1, lsl r12
-	asr 	r1, r1, r2
-	bx	    lr
+    subs    r3, r2, #32
+    rsb     r12, r2, #32
+    lsrmi   r0, r0, r2
+    asrpl   r0, r1, r3
+    orrmi   r0, r0, r1, lsl r12
+    asr     r1, r1, r2
+    bx      lr
--- source/llsl.s
+++ source/llsl.s
@@ -11,10 +11,10 @@
     .global __aeabi_llsl
     .type __aeabi_llsl STT_FUNC
 __aeabi_llsl:
-	subs	r3, r2, #32
-	rsb	    r12, r2, #32
-	lslmi	r1, r1, r2
-	lslpl	r1, r0, r3
-	orrmi	r1, r1, r0, lsr r12
-	lsl	    r0, r0, r2
-	bx	    lr
+    subs    r3, r2, #32
+    rsb     r12, r2, #32
+    lslmi   r1, r1, r2
+    lslpl   r1, r0, r3
+    orrmi   r1, r1, r0, lsr r12
+    lsl     r0, r0, r2
+    bx      lr
