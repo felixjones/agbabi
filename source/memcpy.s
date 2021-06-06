734b89217075f86905cc85cea62fdd40d1c7ee96
Felix Jones <felix@felixjones.co.uk>
2021-06-06 20:53:16 +0200

    Re-wrote memcpy to use @JoaoBaptMG carry & sign bit test

--- source/memcpy.s
+++ source/memcpy.s
@@ -6,7 +6,7 @@
 @   (void *dest, const void *src, size_t n)
 @ memcpy8 is an alias of memcpy4
 @ memcpy4 dest & src are word-aligned
-@ memcpy might not be word-aligned
+@ __agbabi_memcpy2 dest & src are half-word-aligned
 @--------------------------------------------------------------------------------
 
     .section .iwram.__aeabi_memcpy, "ax", %progbits
@@ -15,20 +15,23 @@
     .global __aeabi_memcpy
     .type __aeabi_memcpy STT_FUNC
 __aeabi_memcpy:
-    and     r3, r0, #3
-    and     r12, r1, #3
-    cmp     r3, r12
-    movne   r12, r2
-    bne     .Lcopy
+    @ Check pointer alignment
+    eor     r3, r1, r0
+    @ JoaoBapt carry & sign bit test
+    movs    r3, r3, lsl #31
+    bmi     .Lcopy1
+    bcs     .Lcopy2
 
-    and     r12, r2, #3
-    sub     r2, r2, r12
-.Lcopy_front:
-    subs    r12, r12, #1
-    ldrhsb  r3, [r1], #1
-    strhsb  r3, [r0], #1
-    bhs     .Lcopy_front
-    @ Fallthrough __aeabi_memcpy8/__aeabi_memcpy4
+.Lcopy4:
+    @ Copy half and byte head
+    rsb     r3, r0, #4
+    movs    r3, r3, lsl #31
+    ldrmib  r3, [r1], #1
+    strmib  r3, [r0], #1
+    submi   r2, r2, #1
+    ldrcsh  r3, [r1], #2
+    strcsh  r3, [r0], #2
+    subcs   r2, r2, #2
 
     .global __aeabi_memcpy8
     .type __aeabi_memcpy8 STT_FUNC
@@ -36,44 +39,71 @@ __aeabi_memcpy8:
     .global __aeabi_memcpy4
     .type __aeabi_memcpy4 STT_FUNC
 __aeabi_memcpy4:
-    and     r12, r2, #3
-    movs    r2, r2, lsr #2
-    beq     .Lcopy
-
-    push    {r11}
-    and     r11, r2, #7
-    movs    r2, r2, lsr #3
-    beq     .Lcopy4
-
-    @ r12 = bytes remaining, r11 = words remaining, r2 = 8 words remaining
+    @ Copy 8 words
+    movs    r12, r2, lsr #5
+    beq     .Lskip32
+    lsl     r3, r12, #5
+    sub     r2, r2, r3
     push    {r4-r10}
-.Lcopy32:
+.LcopyWords8:
     ldmia   r1!, {r3-r10}
     stmia   r0!, {r3-r10}
-    subs    r2, #1
-    bne     .Lcopy32
+    subs    r12, r12, #1
+    bne     .LcopyWords8
     pop     {r4-r10}
-.Lcopy4:
-    subs    r11, #1
+.Lskip32:
+
+    @ Copy words
+    movs    r12, r2, lsr #2
+.LcopyWords:
+    subs    r12, r12, #1
     ldrhs   r3, [r1], #4
     strhs   r3, [r0], #4
-    bhs     .Lcopy4
-    pop     {r11}
-.Lcopy:
-    subs    r12, r12, #1
-    ldrhsb  r3, [r1], #1
-    strhsb  r3, [r0], #1
-    bhs     .Lcopy
+    bhs     .LcopyWords
+
+    @ Copy half and byte tail
+    movs    r3, r2, lsl #31
+    ldrcsh  r3, [r1], #2
+    strcsh  r3, [r0], #2
+    ldrmib  r3, [r1]
+    strmib  r3, [r0]
     bx      lr
 
-    .section .ewram, "ax", %progbits
-    .align 2
-    .thumb
+.Lcopy2:
+    @ Copy byte head
+    tst     r0, #1
+    ldrneb  r3, [r1], #1
+    strneb  r3, [r0], #1
+    subne   r2, r2, #1
+
+    .global __agbabi_memcpy2
+    .type __agbabi_memcpy2 STT_FUNC
+__agbabi_memcpy2:
+    @ Copy halves
+    movs    r12, r2, lsr #1
+.LcopyHalves:
+    subs    r12, r12, #1
+    ldrhsh  r3, [r1], #2
+    strhsh  r3, [r0], #2
+    bhs     .LcopyHalves
+
+    @ Copy byte tail
+    tst     r2, #1
+    ldrneb  r3, [r1]
+    strneb  r3, [r0]
+    bx      lr
+
+.Lcopy1:
+    subs    r2, r2, #1
+    ldrhsb  r3, [r1], #1
+    strhsb  r3, [r0], #1
+    bhs     .Lcopy1
+    bx      lr
+
     .global memcpy
     .type memcpy STT_FUNC
 memcpy:
     push    {r0, lr}
     bl      __aeabi_memcpy
-    pop     {r0}
-    pop     {r1}
-    bx      r1
+    pop     {r0, lr}
+    bx      lr
