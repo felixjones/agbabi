f3028c7957976911996e62ec59209dad4523bea1
Felix Jones <felix@felixjones.co.uk>
2021-06-10 23:06:57 +0200

    Re-wrote memset

--- source/memset.s
+++ source/memset.s
@@ -18,16 +18,38 @@
     .section .iwram.__aeabi_memset, "ax", %progbits
     .align 2
     .arm
+    .global __aeabi_memclr
+    .type __aeabi_memclr STT_FUNC
+__aeabi_memclr:
+    mov     r2, #0
+    b       .LskipShifts
+
     .global __aeabi_memset
     .type __aeabi_memset STT_FUNC
 __aeabi_memset:
-    and     r3, r0, #3
-    sub     r1, r1, r3
-.Lcopy_front:
-    subs    r3, r3, #1
-    strhsb  r2, [r0], #1
-    bhs     .Lcopy_front
-    @ Fallthrough __aeabi_memset8/__aeabi_memset4
+    mov     r2, r2, lsl #24
+    orr     r2, r2, r2, lsr #8
+    orr     r2, r2, r2, lsr #16
+
+.LskipShifts:
+    @ JoaoBapt carry & sign bit test
+    rsb     r3, r0, #4
+    movs    r3, r3, lsl #31
+    @ Set half and byte head
+    strmib  r2, [r0], #1
+    submi   r1, r1, #1
+    strcsh  r2, [r0], #2
+    subcs   r1, r1, #2
+    b       __agbabi_wordset4
+
+    .global __aeabi_memclr8
+    .type __aeabi_memclr8 STT_FUNC
+__aeabi_memclr8:
+    .global __aeabi_memclr4
+    .type __aeabi_memclr4 STT_FUNC
+__aeabi_memclr4:
+    mov     r2, #0
+    b       __agbabi_wordset4
 
     .global __aeabi_memset8
     .type __aeabi_memset8 STT_FUNC
@@ -42,76 +64,46 @@ __aeabi_memset4:
     .global __agbabi_wordset4
     .type __agbabi_wordset4 STT_FUNC
 __agbabi_wordset4:
-    and     r3, r1, #3
-    movs    r1, r1, lsr #2
-    beq     .Lset
-
-    and     r12, r1, #7
-    movs    r1, r1, lsr #3
-    beq     .Lset4
-
-    @ r3 = bytes remaining, r12 = words remaining, r1 = 8 words remaining
-    push    {r4-r10}
+    @ Set 8 words
+    movs    r12, r1, lsr #5
+    beq     .Lskip32
+    lsl     r3, r12, #5
+    sub     r1, r1, r3
+    push    {r4-r9}
+    mov     r3, r2
     mov     r4, r2
     mov     r5, r2
     mov     r6, r2
     mov     r7, r2
     mov     r8, r2
     mov     r9, r2
-    mov     r10, r2
-.Lset32:
-    stmia   r0!, {r2, r4-r10}
-    subs    r1, #1
-    bne     .Lset32
-    pop     {r4-r10}
-.Lset4:
-    subs    r12, #1
+.LsetWords8:
+    stmia   r0!, {r2-r9}
+    subs    r12, r12, #1
+    bne     .LsetWords8
+    pop     {r4-r9}
+.Lskip32:
+
+    @ Set words
+    movs    r12, r1, lsr #2
+.LsetWords:
+    subs    r12, r12, #1
     strhs   r2, [r0], #4
-    bhs     .Lset4
-.Lset:
-    subs    r3, r3, #1
-    strhsb  r2, [r0], #1
-    mov     r2, r2, lsr #8
-    bhs     .Lset
+    bhs     .LsetWords
+
+    @ Set half and byte tail
+    movs    r3, r1, lsl #31
+    strcsh  r2, [r0], #2
+    strmib  r2, [r0]
     bx      lr
 
-    .section .ewram.__aeabi_memclr8, "ax", %progbits
-    .align 2
-    .thumb
-    .global __aeabi_memclr8
-    .type __aeabi_memclr8 STT_FUNC
-__aeabi_memclr8:
-    .section .ewram.__aeabi_memclr4, "ax", %progbits
-    .align 2
-    .thumb
-    .global __aeabi_memclr4
-    .type __aeabi_memclr4 STT_FUNC
-__aeabi_memclr4:
-    movs    r2, #0
-    ldr     r3, =__agbabi_wordset4
-    bx      r3
-
-    .section .ewram.__aeabi_memclr, "ax", %progbits
-    .align 2
-    .thumb
-    .global __aeabi_memclr
-    .type __aeabi_memclr STT_FUNC
-__aeabi_memclr:
-    movs    r2, #0
-    ldr     r3, =__aeabi_memset
-    bx      r3
-
-    .section .ewram.memset, "ax", %progbits
-    .align 2
-    .thumb
     .global memset
     .type memset STT_FUNC
 memset:
-    movs    r3, r2
-    movs    r2, r1
-    movs    r1, r3
+    mov     r3, r1
+    mov     r1, r2
+    mov     r2, r3
     push    {r0, lr}
     bl      __aeabi_memset
-    pop     {r0}
-    pop     {r1}
-    bx      r1
+    pop     {r0, lr}
+    bx      lr
