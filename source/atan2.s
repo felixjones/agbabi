2fc520dce37c963b0e8ebc6bcdfb118c6d045253
Felix Jones <felix@felixjones.co.uk>
2022-06-20 23:10:04 +0200

    Fix __agbabi_atan2(-1, -1) producing incorrect results

--- source/atan2.s
+++ source/atan2.s
@@ -36,11 +36,14 @@ __agbabi_atan2:
     movlt   r2, #6
     movge   r2, #2
 
+    movlt   r4, #4
     movge   r4, #0
+
     cmp     r1, #0
     rsble   r0, r1, #0 // t = -x
     movle   r1, r3 // x = y
     movle   r3, r0 // y = t
+
     movle   r4, r2
 
     cmp     r1, r3
@@ -48,9 +51,10 @@ __agbabi_atan2:
     addle   r1, r1, r3 // x = x + y
     movle   r3, r2 // y = t
 
+    addle   r4, r4, #1
+
     // t = y / x
     lsl     r0, r3, #15
-    addle   r4, r4, #1
     bl      __aeabi_idiv
 
     // t2 = ((-t) * t) >> 15
@@ -60,24 +64,24 @@ __agbabi_atan2:
 
     add     r3, r2, r2, lsl #3
     rsb     r3, r2, r3, lsl #3
-    asr     r3, r3, #11
+    asr     r3, r3, #0x000B
     add     r3, r3, #0x1000
-    add     r3, r3, #41
+    add     r3, r3, #0x0029
 
     mul     r3, r2, r3
-    asr     r3, r3, #15
+    asr     r3, r3, #0x000F
     add     r3, r3, #0x1F00
-    add     r3, r3, #11
+    add     r3, r3, #0x000B
 
     mul     r3, r2, r3
-    asr     r3, r3, #15
+    asr     r3, r3, #0x000F
     add     r3, r3, #0x3640
-    add     r3, r3, #12
+    add     r3, r3, #0x000C
 
     mul     r3, r2, r3
-    asr     r3, r3, #15
+    asr     r3, r3, #0x000F
     add     r3, r3, #0xA200
-    add     r3, r3, #252
+    add     r3, r3, #0x00FC
 
     mul     r3, r0, r3
     asr     r3, r3, #15
@@ -85,9 +89,13 @@ __agbabi_atan2:
     // octant *= pi/4
     lsl     r1, r4, #(BAM_BITS - 3)
 
-    // +4 rounds to nearest integer
-    add     r3, r3, #4
+    // +0.5 rounds to nearest integer
+    add     r3, r3, #(1 << (17 - BAM_BITS))
+
     add     r0, r1, r3, asr #(18 - BAM_BITS)
+
+    // Clear sign bit
+    bic     r0, r0, #(1 << BAM_BITS)
 
     pop     {r4, lr}
     bx      lr
