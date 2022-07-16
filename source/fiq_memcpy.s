c6cb5da4bde251d1951e25c20ed2e9ec481cddde
Felix Jones <felix@felixjones.co.uk>
2022-07-16 12:12:34 +0200

    Removed impossible to reach branch of fiq_memcpy4 (address must be 4 byte aligned)

--- source/fiq_memcpy.s
+++ source/fiq_memcpy.s
@@ -42,13 +42,10 @@ __agbabi_fiq_memcpy4:
     bxeq    lr
 
 .Lcopy_words:
-    tst     r2, #4
-    blt     .Lcopy_halves
-.Lloop_4:
     subs    r2, r2, #4
     ldrge   r3, [r1], #4
     strge   r3, [r0], #4
-    bgt     .Lloop_4
+    bgt     .Lcopy_words
     bxeq    lr
 
     // Copy byte & half tail
@@ -59,27 +56,6 @@ __agbabi_fiq_memcpy4:
     // Copy byte
     ldrmib  r3, [r1]
     strmib  r3, [r0]
-    bx      lr
-
-.Lcopy_halves:
-    // Copy byte head to align
-    tst     r0, #1
-    ldrneb  r3, [r1], #1
-    strneb  r3, [r0], #1
-    subne   r2, r2, #1
-    // r0, r1 are now half aligned
-
-.Lloop_2:
-    subs    r2, r2, #2
-    ldrgeh  r3, [r1], #2
-    strgeh  r3, [r0], #2
-    bgt     .Lloop_2
-    bxeq    lr
-
-    // Copy byte tail
-    adds    r2, r2, #1
-    ldreqb  r3, [r1]
-    streqb  r3, [r0]
     bx      lr
 
     .section .iwram.__agbabi_fiq_memcpy4x4, "ax", %progbits
