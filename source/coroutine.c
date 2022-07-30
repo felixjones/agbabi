c93228521a2942da0cc7e2d27a6fc94b736d491c
felixjones <felix.jones@mojang.com>
2022-07-31 00:43:47 +0100

    Simplified coro stack assignment

--- source/coroutine.c
+++ source/coroutine.c
@@ -19,11 +19,11 @@ void __agbabi_coro_make(agbabi_coro_t* __restrict__ coro, void* __restrict__ sp_
     unsigned int alignedTop = ((unsigned int) sp_top) & ~0x7;
 
     unsigned int* stack = (unsigned int*) alignedTop;
-    stack -= 2; // Allocate space on stack for pointer to self, and entry procedure
-    stack[0] = (unsigned int) coro;
-    stack[1] = (unsigned int) coproc;
-    stack -= 10; // Allocate space for storing r4-r12, lr (r12 for alignment)
+    // Allocate space for storing r4-r12 (r12 for alignment)
+    stack -= 12; // Allocate space on stack for pointer to lr, self, and entry procedure
     stack[9] = (unsigned int) __agbabi_coro_pop;
+    stack[10] = (unsigned int) coro;
+    stack[11] = (unsigned int) coproc;
 
     coro->arm_sp = (unsigned int) stack;
     coro->joined = 0; // Clear joined flag (ready to start)
