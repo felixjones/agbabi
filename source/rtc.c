a4404b15c74c2b7dae3a555cdb183ade9959a636
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2023-11-18 14:42:17 +0100

    Make setdatetime work for real carts and ignore pm flag

--- source/rtc.c
+++ source/rtc.c
@@ -48,11 +48,18 @@
 #define INIT_EPOWER  (0x01)
 #define INIT_E12HOUR (0x02)
 
+// Mask out data not needed from date and time
+#define PM_TIME_FLAG (0x00000080)
+#define HOUR_TIME_24_MASK (0x007F7F1F)
+#define HOUR_TIME_12_MASK (0x007F7F8F)
+#define DATE_MASK (0x073F1FFF)
+
 /* Compiler hacks */
 #define assume(cond) do { if (!(cond)) __builtin_unreachable(); } while (0)
 #define unlikely(cond) __builtin_expect(!!(cond), 0)
 
 static unsigned int rtc_read(unsigned int len);
+static void rtc_write(unsigned int data, unsigned int len);
 static void rtc_cmd(unsigned int cmd);
 static void rtc_cmd_arg(unsigned int cmd, unsigned int data, unsigned int len);
 static void rtc_cmd_arg_datetime(unsigned int cmd, __agbabi_datetime_t datetime);
@@ -90,6 +97,21 @@ static unsigned int rtc_read(unsigned int len) {
     return data;
 }
 
+static void rtc_write(unsigned int data, unsigned int len) {
+    for (unsigned int ii = 0; ii < len; ++ii) {
+        unsigned int output = data >> ii & 1;
+        __asm__ volatile (
+            "strh    %[select], [%[gpio]]"  "\n\t"
+            "strh    %[select], [%[gpio]]"  "\n\t"
+            "strh    %[select], [%[gpio]]"  "\n\t"
+            "strh    %[clock], [%[gpio]]"
+            ::  [gpio]"l"(MMIO_GPIO_DATA),
+                [select]"l"(RTC_CS | output << 1),
+                [clock]"l"(RTC_CS | output << 1 | RTC_SCK)
+        );
+    }
+}
+
 static void rtc_cmd(const unsigned int cmd) {
     *MMIO_GPIO_WRITE_ENABLE = RTC_CS | RTC_DATA | RTC_SCK;
 
@@ -109,66 +131,20 @@ static void rtc_cmd(const unsigned int cmd) {
 }
 
 static void rtc_cmd_arg(const unsigned int cmd, unsigned int data, unsigned int len) {
-    *MMIO_GPIO_WRITE_ENABLE = RTC_CS | RTC_DATA | RTC_SCK;
-
-    unsigned ii = 8;
-    while (ii--) {
-        unsigned int output = cmd >> ii & 1;
-        __asm__ volatile (
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[clock], [%[gpio]]"
-            ::  [gpio]"l"(MMIO_GPIO_DATA),
-                [select]"l"(RTC_CS | output << 1),
-                [clock]"l"(RTC_CS | output << 1 | RTC_SCK)
-        );
-    }
+    rtc_cmd(cmd);
 
     assume(len > 0 && len <= 32);
-    for (ii = 0; ii < len; ++ii) {
-        unsigned int output = data >> ii & 1;
-        __asm__ volatile (
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[clock], [%[gpio]]"
-            ::  [gpio]"l"(MMIO_GPIO_DATA),
-                [select]"l"(RTC_CS | output << 1),
-                [clock]"l"(RTC_CS | output << 1 | RTC_SCK)
-        );
-    }
+    rtc_write(data, len);
 }
 
 static void rtc_cmd_arg_datetime(unsigned int cmd, __agbabi_datetime_t datetime) {
-    *MMIO_GPIO_WRITE_ENABLE = RTC_CS | RTC_DATA | RTC_SCK;
+    rtc_cmd(cmd);
 
-    unsigned ii = 8;
-    while (ii--) {
-        unsigned int output = cmd >> ii & 1;
-        __asm__ volatile (
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[clock], [%[gpio]]"
-            ::  [gpio]"l"(MMIO_GPIO_DATA),
-                [select]"l"(RTC_CS | output << 1),
-                [clock]"l"(RTC_CS | output << 1 | RTC_SCK)
-        );
-    }
+    unsigned int date = datetime[0];
+    unsigned int time = datetime[1];
 
-    for (ii = 0; ii < 55; ++ii) {
-        unsigned int output = (datetime >> ii)[0] & 1;
-        __asm__ volatile (
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[select], [%[gpio]]"  "\n\t"
-            "strh    %[clock], [%[gpio]]"
-            ::  [gpio]"l"(MMIO_GPIO_DATA),
-                [select]"l"(RTC_CS | output << 1),
-                [clock]"l"(RTC_CS | output << 1 | RTC_SCK)
-        );
-    }
+    rtc_write(date, 32);
+    rtc_write(time, 24);
 }
 
 static void rtc_reset(void) {
@@ -251,7 +227,7 @@ unsigned int __agbabi_rtc_time(void) {
     *MMIO_GPIO_DATA = RTC_CS | RTC_SCK;
 
     rtc_cmd(cmd_time);
-    const unsigned int time = rtc_read(23);
+    const unsigned int time = rtc_read(24) & HOUR_TIME_24_MASK;
 
     *MMIO_GPIO_DATA = RTC_SCK;
     *MMIO_GPIO_DATA = RTC_SCK;
@@ -266,7 +242,7 @@ void __agbabi_rtc_settime(const unsigned int time) {
     *MMIO_GPIO_DATA = RTC_SCK;
     *MMIO_GPIO_DATA = RTC_CS | RTC_SCK;
 
-    rtc_cmd_arg(cmd_time, time, 23);
+    rtc_cmd_arg(cmd_time, time, 24);
 
     *MMIO_GPIO_DATA = RTC_SCK;
     *MMIO_GPIO_DATA = RTC_SCK;
@@ -281,8 +257,8 @@ __agbabi_datetime_t __agbabi_rtc_datetime(void) {
 
     rtc_cmd(cmd_datetime);
     const __agbabi_datetime_t datetime = {
-        rtc_read(32),
-        rtc_read(23)
+        rtc_read(32) & DATE_MASK,
+        rtc_read(24) & HOUR_TIME_24_MASK
     };
 
     *MMIO_GPIO_DATA = RTC_SCK;
